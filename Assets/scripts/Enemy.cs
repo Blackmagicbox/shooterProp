0aa8195f0378e902f74ac985b725e60d7d59b123
Rafael <rafa@blackmagicbox.com.br>
2019-12-11 00:53:38 +0100

    Feat enemy unity erratic movement behaviour

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -2,15 +2,26 @@
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField]
+    private float speed = 4;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0,5.95f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform transform1 = transform;
+        transform1.Translate(Time.deltaTime * speed * Vector3.down);
+
+
+        if (transform1.position.y <= -6.17)
+        {
+            transform1.position = new Vector3(Random.Range(-10f, 10f), 5.95f, 0);
+        } ;
         
     }
 }
