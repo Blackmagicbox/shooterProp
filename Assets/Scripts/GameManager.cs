818eaea5c98082fb8c9ab87c6091803a32ded4c8
Rafael <rafa@blackmagicbox.com.br>
2020-02-20 23:44:01 +0100

    Add Game manager and implement gameOver method

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private bool isGameOver;
+
+    private void Update()
+    {
+        if (isGameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Scenes/Game");
+        }
+    }
+
+
+    public void GameOver()
+    {
+        isGameOver = true;
+    }
+}
